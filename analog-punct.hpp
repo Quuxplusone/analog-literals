e165cbd3b78626611818729992c4ee79bae9e632
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2013-01-04 16:48:18 -0800

    Exhaustively test Wx2x1 cuboids.
    These tests might still not be 100% exhaustive, but they must
    be pretty close!

--- analog-punct.hpp
+++ analog-punct.hpp
@@ -201,6 +201,9 @@ namespace analog_literals {
 
   bangs<1,Lsym> operator! (Lsym) { return gen(); }
 
+  osidesobottomo<0,0> operator* (osym, osym) { return gen(); }
+  osidesobottomo<0,0> operator* (line<0>) { return gen(); }
+
   // odiag represents *!L|L|L
   template <int l> struct odiag {};
   odiag<1> operator* (bangs<1,Lsym>) { return gen(); }
@@ -209,9 +212,15 @@ namespace analog_literals {
   template <int l> dashes<1, odiag<l> > operator- (odiag<l>) { return gen(); }
   template <int l> dashes<2, odiag<l> > operator-- (odiag<l>) { return gen(); }
 
+  // baseline represents L|!L|!*~~o
+  template <int l, int b> struct baseline {};
+  template <int b> baseline<1,b> operator| (Lsym, bangs<1, line<b> >) { return gen(); }
+  template <int l, int b> baseline<l+1,b> operator| (Lsym, bangs<1, baseline<l,b> >) { return gen(); }
+
   // otopodiag represents *~~*!L|L|L
   template <int t, int l> struct otopodiag {};
   otopodiag<0,1> operator* (odiag<1>) { return gen(); }
+  otopodiag<0,1> operator* (osym, bangs<1,Lsym>) { return gen(); }
   template <int t> otopodiag<t,1> operator* (odashes<t>, bangs<1,Lsym>) { return gen(); }
   template <int t> otopodiag<t,1> operator* (dashes<t, odiag<1> >) { return gen(); }
   otopodiag<1,1> operator- (osym, odiag<1>) { return gen(); }
@@ -234,23 +243,36 @@ namespace analog_literals {
   template <int l> dashes<1, oobase<l> > operator- (oobase<l>) { return gen(); }
   template <int l> dashes<2, oobase<l> > operator-- (oobase<l>) { return gen(); }
 
+  template <int t> dashes<t, oobase<1> > operator* (dashes<t,osym>, bangs<2,Lsym>) { return gen(); }
+
   // otopoobase represents *~~**!!L|!L|!L
   template <int t, int l> struct otopoobase {};
+  otopoobase<0,1> operator* (osym, obase<1>) { return gen(); }
   template <int l> otopoobase<0,l> operator* (oobase<l>) { return gen(); }
   template <int t> otopoobase<t,1> operator* (line<t>, bangs<2,Lsym>) { return gen(); }
   template <int t> otopoobase<t,1> operator* (odashes<t>, obase<1>) { return gen(); }
   template <int t, int l> otopoobase<t,l> operator* (dashes<t, oobase<l> >) { return gen(); }
-  template <int l> otopoobase<1,l> operator- (osym, oobase<l>) { return gen(); }
-  template <int t, int l> otopoobase<t+1,l> operator- (osym, dashes<t, oobase<l> >) { return gen(); }
+  otopoobase<1,1> operator- (osym, oobase<1>) { return gen(); }
+  template <int t> otopoobase<t+1,1> operator- (odashes<t>, oobase<1>) { return gen(); }
+  template <int t> otopoobase<t+1,1> operator- (osym, dashes<t, oobase<1> >) { return gen(); }
+  template <int p, int q> otopoobase<p+q+1,1> operator- (odashes<p>, dashes<q, oobase<1> >) { return gen(); }
 
   // otopodiagotopoobase represents o--o!L|L|L|o--o*!!L|!L|!L
   template <int t, int d, int b> struct otopodiagotopoobase {};
+  otopodiagotopoobase<0,1,0> operator| (otopodiag<0,1>, osidesobottomo<0,0>) { return gen(); }
   template <int t, int d, int b> otopodiagotopoobase<t,d,b> operator| (otopodiag<t,d>, otopoobase<t,b>) { return gen(); }
   template <int t, int d, int b> otopodiagotopoobase<t,d,b+1> operator| (otopodiagotopoobase<t,d,b>, bangs<1,Lsym>) { return gen(); }
-  template <int t, int d> otopodiagotopoobase<t,d,0> operator| (otopodiag<t, d>, otoposidesobottom<t, 0, 0>) { return gen(); }
+  template <int t, int d> otopodiagotopoobase<t,d,0> operator| (otopodiag<t,d>, otoposidesobottom<t,0,0>) { return gen(); }
+  template <int t, int d, int b> otopodiagotopoobase<t,d,b+1> operator|| (otopodiagotopoobase<t,d,b>, Lsym) { return gen(); }
 
   // Constructing a cuboid: (o-~o|L|o-~o|!L) | (!o-~o)
   template <int t, int d> cuboid<t,d+1,d> operator| (otopodiagotopoobase<t,d,d>, bangs<1,line<t> >) { return gen(); }
+
+  // Constructing a cuboid: (oo|L|oo|!L) || (oo)
+  template <int d> cuboid<0,d+1,d> operator|| (otopodiagotopoobase<0,d,d>, line<0>) { return gen(); }
+
+  // Constructing a cuboid: (oo|L|oo) || (L|!oo)
+  template <int d> cuboid<0,d+1,d> operator|| (otopodiagotopoobase<0,d,0>, baseline<d,0>) { return gen(); }
 
 } // analog_literals
 
@@ -445,21 +467,47 @@ int main ()
     (rectangle<5,1>)(o-~~~~o |! o---~~o);     (rectangle<5,2>)(o-~~~~o |!!! o---~~o);
     (rectangle<5,1>)(o-~~~~o |! o-~~~~o);     (rectangle<5,2>)(o-~~~~o |!!! o-~~~~o);
 
-(cuboid<3,2,1>)(
-*~~~*
-!L   \
-| *~~~*
-* !   !
- L|   !
-  *~~~o);
+  (cuboid<0,2,1>)(**!L|***!!L|!*o);
+  (cuboid<0,2,1>)(**!L|**o|!L|!*o);
+  (cuboid<0,2,1>)(**!L|*o*!!L|!*o);
+  (cuboid<0,2,1>)(**!L|o**!!L|!*o);
+  (cuboid<0,2,1>)(**!L|o*o|!L|!*o);       
+  (cuboid<0,2,1>)(*o|L|***!!L|!*o);
+  (cuboid<0,2,1>)(*o|L|**o|!L|!*o);
+  (cuboid<0,2,1>)(*o|L|*o*!!L|!*o);
+  (cuboid<0,2,1>)(*o|L|o**!!L|!*o);
+  (cuboid<0,2,1>)(*o|L|o*o|!L|!*o);
+  (cuboid<0,2,1>)(o*!L|***!!L|!*o);
+  (cuboid<0,2,1>)(o*!L|**o|!L|!*o);
+  (cuboid<0,2,1>)(o*!L|*o*!!L|!*o);
+  (cuboid<0,2,1>)(o*!L|o**!!L|!*o);
+  (cuboid<0,2,1>)(o*!L|o*o|!L|!*o);
+  (cuboid<0,2,1>)(**!L|***!!L||*o);
+  (cuboid<0,2,1>)(**!L|**o||L|!*o);
+  (cuboid<0,2,1>)(**!L|**o||L||*o);
 
-(cuboid<3,2,1>)(
-o-~-*
-!L   \
-| *~~~*
-* !   !
- L|   !
-  o---o);
+  (cuboid<1,2,1>)(*~*!L|*~**!!L|!*~o);
+  (cuboid<1,2,1>)(*~*!L|*~**!!L|!o-o);
+  (cuboid<1,2,1>)(*~*!L|*~*o|!L|!*~o);
+  (cuboid<1,2,1>)(*~*!L|*~o*!!L|!*~o);
+  (cuboid<1,2,1>)(*~*!L|o-**!!L|!*~o);
+  (cuboid<1,2,1>)(*~*!L|o-*o|!L|!*~o);
+  (cuboid<1,2,1>)(*~*!L|o-o*!!L|!*~o);
+  (cuboid<1,2,1>)(*~o|L|*~**!!L|!*~o);
+  (cuboid<1,2,1>)(o-o|L|o-o*!!L|!o-o);
+
+  (cuboid<5,2,1>)(*~~~~~*!L|*~~~~~**!!L|!*~~~~~o);
+  (cuboid<5,2,1>)(*~~~~~*!L|*~~~~~**!!L|!o-~~~~o);
+  (cuboid<5,2,1>)(*~~~~~*!L|*~~~~~**!!L|!o---~~o);
+  (cuboid<5,2,1>)(*~~~~~*!L|*~~~~~*o|!L|!*~~~~~o);
+  (cuboid<5,2,1>)(*~~~~~*!L|o-~~~~*o|!L|!*~~~~~o);
+  (cuboid<5,2,1>)(*~~~~~*!L|o---~~*o|!L|!*~~~~~o);
+  (cuboid<5,2,1>)(*~~~~~*!L|*~~~~~o*!!L|!*~~~~~o);
+  (cuboid<5,2,1>)(*~~~~~*!L|o-~~~~o*!!L|!*~~~~~o);
+  (cuboid<5,2,1>)(*~~~~~*!L|o---~~o*!!L|!*~~~~~o);
+  (cuboid<5,2,1>)(*~~~~~*!L|o-~~~~**!!L|!*~~~~~o);
+  (cuboid<5,2,1>)(*~~~~~*!L|o---~~**!!L|!o-~~~~o);
+  (cuboid<5,2,1>)(*~~~~~*!L|o-----**!!L|!o---~~o);
 
 }
 
